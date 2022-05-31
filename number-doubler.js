b4e25d076c679d155870224769e5cbc726fa5db2
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-05-31 15:53:08 -0700

    number doubler

--- number-doubler.js
+++ number-doubler.js
@@ -6,14 +6,15 @@ let varOne = true;
 let numOne = 0;
 numOne = Number(prompt("Enter number: "));
 
-while (varOne = true) {
+while (varOne === true) {
 
     numOne = numOne * 2;
     if (numOne <= 100) {
         console.log(numOne)
     }
     
-    if (numOne >= 100) {
+    else if (numOne >= 100) {
         varOne = false
-    }
-} 
+        console.log(numOne)
+    } 
+}  
