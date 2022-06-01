ee1ce584b39617c7ba1d4e9334abeb53e8cee4de
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-06-01 11:59:22 -0700

    fib

--- fib.js
+++ fib.js
@@ -6,21 +6,8 @@ const prompt = require('prompt-sync')({
 
 let n = prompt("Enter a value for 'n': ");
 
-
-// 1st iteration
-// 0 1 1 2
-// p c t
-
-// 2nd iteration
-// 0 1 1 2
-//   p c t
-
-// 3rd iteration
-// 0 1 1 2 3
-//     p c t
-
-let prevNum = 0;
-let currentNum = 1;
+let prevNum = 0; // 
+let currentNum = 1; //
 let total = 0; // prevNum + currentNum; 
 
 console.log(prevNum); // accounts for the first 0 in the sequence
